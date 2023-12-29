edd4ef261fcd7ab99a3f33bb335cf77226578bad
Taycanstar <diminunez06@gmail.com>
2023-12-29 16:16:53 -0500

    Initial Commit

---
+++ loggler/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  loggler
+//
+//  Created by Dimerson Nunez on 12/29/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ loggler/logglerApp.swift
@@ -0,0 +1,17 @@
+//
+//  logglerApp.swift
+//  loggler
+//
+//  Created by Dimerson Nunez on 12/29/23.
+//
+
+import SwiftUI
+
+@main
+struct logglerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
